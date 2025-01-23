b5dde53808358e971d65427a8b6b0dd68c36b7c1
Ramy <ramy3@usf.edu>
2025-01-23 16:41:54 -0500

    first commit

---
+++ cc_1.js
@@ -0,0 +1,42 @@
+// Task 1: Employee Information
+let employeeName = "Ramy Abdelbaky";
+const employeeID = 4444;
+var isActive = true;
+
+console.log ("EmployeeName:", employeeName, "- type:", typeof employeeName);
+console.log ("EmployeeID:", employeeID, "- type:", typeof employeeID)
+console.log ("Is Active:", isActive, "- type:", typeof isActive)
+
+// Task 2: Product Details 
+let productName = "Beanie";
+const productPrice = 25;
+var  isAvailable = true;
+
+console.log("Product Name:", productName, "- Type:", typeof productName)
+console.log("ProductPrice:", productPrice, "- Type:", typeof productPrice)
+console.log("Is Available:", isAvailable, "- Type:", typeof isAvailable)
+
+// Task 3: Financial Transactions
+let accountBalance = 100
+accountBalance -= 25
+console.log (accountBalance)
+accountBalance +=12
+console.log (accountBalance)
+accountBalance *=1
+console.log (accountBalance)
+accountBalance /=1
+console.log (accountBalance)
+
+// Task 4: Customer Messaging 
+let customerName = "Ramy Abdelbaky";
+let welcomeMessage = "Hey There" + customerName + ",we hope to see you again"
+console.log(welcomeMessage);
+
+// Task5: Access Control
+let isLoggedIn = true;
+if (isLoggedIn) {
+    console.log("Welcomome Back!");
+}else 
+{console.log("Unsuccseful Sign In!");
+
+}
